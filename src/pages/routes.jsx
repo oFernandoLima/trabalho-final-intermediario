41094d69aa77c9b1986ee30ca54c8dc94b8cdca7
@Samuel.Santos.Avanz <samuel.santos@avanz.com.br>
2025-01-27 08:49:54 -0300

    feat: adicionando rotas da pagina layout no arquivo routes

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,14 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignIn from "./SignIn";
 import Home from "./Home";
+import DashboardLayoutBasic from "../components/Layout"
+import { Products } from "./Products"
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/sign-in",
     element: <SignIn />,
   },
   {
-    path: "/",
-    element: <Home />,
+    path: "/administrador",
+    element: <DashboardLayoutBasic />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+    ],
   },
-]);
+])
